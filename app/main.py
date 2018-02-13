de9d55e3f811df8d7cd68708a545b68a2e7be424
magicyu90 <magicyu90@gmail.com>
2018-02-13 14:55:23 +0800

    add requirements.txt

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ def create_app():
     CORS(app)  # 跨域支持
     api_bp = Blueprint('api', __name__)
     # swagger支持
-    api = swagger.docs(Api(api_bp), apiVersion='0.1', resourcePath='/', description='EMDP_API', api_spec_url='/swagger')
+    api = swagger.docs(Api(api_bp), apiVersion='0.1', resourcePath='/',
+                       description='EMDP_API', api_spec_url='/swagger')
     # 添加对应资源
     api.add_resource(EquipmentController, '/equipments')
 
